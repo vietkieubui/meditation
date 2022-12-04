45463b14ef6cbc483dacbca7fe01c18daadc28d7
Bùi Việt Kiều <kieubv2000@gmail.com>
2022-12-04 22:31:18 +0700

    init backend

---
+++ backend/src/app.module.ts
@@ -0,0 +1,27 @@
+import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLDriver, GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MulterModule } from '@nestjs/platform-express';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TracksModule } from './tracks/tracks.module';
+
+@Module({
+  imports: [
+    MulterModule.register({
+      dest: './files',
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:EOENDoVfISawKJ5p@cluster0.jwdsz5d.mongodb.net/?retryWrites=true&w=majority`,
+    ),
+    TracksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}

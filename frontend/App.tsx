b4b7fc4541ad1a7dfe37927586ec21e34fe87690
Nguyễn Hồng Đức <87305657+hoanKma@users.noreply.github.com>
2023-01-04 10:21:58 +0700

    sua ip

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -11,7 +11,7 @@ import {RecoilRoot} from 'recoil';
 import AuthContextProvider from './app/context/AuthContextProvider';
 import MainApp from './app/MainApp';
 
-export const HOST = '192.168.31.74';
+export const HOST = '10.29.248.114';
 export const PORT = '3001';
 
 export const queryClient = new QueryClient({
